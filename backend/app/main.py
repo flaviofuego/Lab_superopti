6c7b313481a1ef4d89d3b8b70535851a3b3547ab
Flavio Arregoces <flavioarregoces@gmail.com>
2025-03-19 15:35:19 -0500

    cors  added
    po

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import punto1_router, punto2_router, punto3_router, punto4_router
 
 app = FastAPI(
@@ -6,11 +8,17 @@ app = FastAPI(
     description="API para el aprendizaje de optimización",
     version="0.1",
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(punto1_router)
 app.include_router(punto2_router)
 app.include_router(punto3_router)
 app.include_router(punto4_router)
+
 
 @app.get("/")
 async def root():
